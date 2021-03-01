527ebc2b49e3a4d4cc807bf9b0d325415973f004
Sebastien PEREZ <neodesign94@hotmail.fr>
2021-03-01 09:25:41 +0100

    index.php

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 require('../inc/fonction.php');
 require('../inc/pdo.php');
+?>
 
+<form class="" action="index.html" method="post">
+  <input type="text" name="" value="click sebastien">
+</form>
