f9616b8467532082b4ba81157ca4222728e7aa69
Lcor76 <Pro@MacBook-Pro-de-mac.local>
2021-03-01 11:38:05 +0100

    actualisation page index

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,22 @@
 <?php
 require('inc/function.php');
+require('inc/pdo.php');
 
 
-include('inc/header.php'); ?>
+include('inc/header.php'); 
 
 
 
 
-<?php include('inc/footer.php');
+
+
+
+
+
+
+
+
+
+
+
+include('inc/footer.php');
