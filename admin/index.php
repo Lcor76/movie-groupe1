322ff109b0357d3add951fd5e10388d641a1db5e
Steven27210 <steven.vastra@gmail.com>
2021-03-01 16:24:00 +0100

    login fin

--- admin/index.php
+++ admin/index.php
@@ -3,17 +3,6 @@ require('../inc/fonction.php');
 require('../inc/pdo.php');
 
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
->>>>>>> ae349dec843d99dba0283023f95fc6b32499e1b8
 include('inc/header.php');
 ?>
 
