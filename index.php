097c7513bf174822c2f67c58bd942cf4f33220c5
Laurent CORBLIN <Pro@MacBook-Pro-de-mac.local>
2021-03-01 09:22:12 +0100

    ajout prenom

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+    <body>
+       
+      </body>
+</body>
+</html>
+
+
 <?php
 
-require('../inc/fonction.php');
-require('../inc/pdo.php');
+
+echo '<h1>Laurent</h1>';
 
