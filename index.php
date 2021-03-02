4b7bb71901d6672ccd547e462a57d1dbd650e170
Lcor76 <Pro@MacBook-Pro-de-mac.local>
2021-03-02 13:59:10 +0100

    tri effectué

--- index.php
+++ index.php
@@ -18,32 +18,87 @@ require('inc/pdo.php')?>
  $movies = $query->fetchall();
 
 
+ $categorys = array('drama','comedy','animation','crime','horror','romance',);
+
+if (!empty($_GET['submitted'])) {
+
+   
+
+    if(!empty($_GET['genres'])) {
+        $genres = $_GET['genres'];
+
+        if(!empty($_GET['year1'])){
+            $year1  = $_GET['year1'];
+            }
+
+            if(!empty($_GET['year2'])){
+            $year2  = $_GET['year2'];
+             }
+
+    } else {
+        $genres = array();
+    }
+
+    $sql = "SELECT * FROM movies_full WHERE  year >= $year1 AND year <= $year2";
+    if(!empty($genres)) {
+        foreach($genres as $genre) {
+            $sql .= " AND genres LIKE :$genre";
+        }
+    }
+
+
+
+    $sql .= " ORDER BY RAND() LIMIT 20";
+
+ 
+    $query = $pdo->prepare($sql);
+
+    if(!empty($genres)) {
+        foreach($genres as $genre) {
+            $query->bindValue(':'.$genre,'%' . $genre . '%',PDO::PARAM_STR);
+           
+        }
+    }
+
+    $query->execute();
+    $movies = $query->fetchAll();
+    
+
+}
+
+
+
 
 
 include('inc/header.php');?>
 
-
-
+<form action="" method="GET">
       <div id="headerfiltre">
         <div class="checkbox">
-           <h1 class="titrefiltre">FILTRER :</h1>
-           <label for="drame">DRAMA<input type="checkbox" class="drame" name="drame"></label>
-           <label for="comedy">COMEDY<input type="checkbox" class="comedy" name="comedy"></label>
-           <label for="action">ACTION<input type="checkbox" class="action" name="action"></label>
-           <label for="crime">CRIME<input type="checkbox" class="crime" name="crime"></label>
-           <label for="horror">HORROR<input type="checkbox" class="horror" name="horror"></label>
-           <label for="romance">ROMANCE<input type="checkbox" class="romance" name="romance"></label>
-           <input id="subfiltre"type="submit" name="" value="FILTRER">
+           <h1 class="titrefiltre">FILTRES :</h1>
+          <?php foreach($categorys as $category)  { ?>
+            <label for="<?= $category; ?>"><?= ucfirst($category); ?><input name="genres[]" type="checkbox" value="<?= $category; ?>"></label>
+          <?php }  ?>
+           
+           <input type="number" name="year1" value="1900" />
+           <input type="number" name="year2" value="2020" />
+           <input id="subfiltre" type="submit" name="submitted" value="FILTRER">
         </div>
+       
+      
       </div>
+    </form>
+   
 
+
+   
       <div class="clear"></div>
 
 <div class="wrap">
      <div id="movies">
          <?php foreach ($movies as $movie) { ?>
              <div id="imagebox">
-                 <a id="affiches" href="details.php?slug=<?= $movie['slug']; ?>" "style=width:auto;height:200px;">
+                 <a style=width:auto;height:200px; id="affiches" href="details.php?slug=<?= $movie['slug']; ?>" >
                     <?php echo imageMovie($movie); ?>
                   </a>
 
